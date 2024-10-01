5d097a4696169c06fec1b17b7bfb68d6b416200a
ALAN DE MEDEIROS <alan_de-medeiros@estudante.sesisenai.org.br>
2024-10-01 19:07:50 -0300

    Teste_Final

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-var tarefas = [];
+let tarefas = [];
 
 function adicionarTarefa() {
     const input = document.getElementById("tarefa-text");
@@ -13,11 +13,11 @@ function adicionarTarefa() {
         id: Math.floor(Math.random() * 1000000),
         text: tarefaTexto,
         completed: false
-    }
+    };
 
     tarefas.push(novaTarefa);
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
-    render()
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+    render();
     input.value = "";
     input.focus();
 }
@@ -26,53 +26,49 @@ function render() {
     const listaTarefas = document.getElementById("lista-tarefa");
     listaTarefas.innerHTML = "";
 
-    for (var i = 0; i < tarefas.length; i++) {
+    tarefas.forEach(tarefa => {
         const li = document.createElement("li");
-        if (tarefas[i].completed === true) {
+        if (tarefa.completed) {
             li.classList.add("completed");
         }
 
         const span = document.createElement("span");
-        span.textContent = tarefas[i].text;
+        span.textContent = tarefa.text;
 
         const concluir = document.createElement("span");
         concluir.textContent = "task_alt";
-        concluir.classList.add("check");
-        concluir.classList.add("material-symbols-outlined")
-        concluir.setAttribute("onclick", `trocaConcluir(${tarefas[i].id})`);
+        concluir.classList.add("check", "material-symbols-outlined");
+        concluir.setAttribute("onclick", `trocaConcluir(${tarefa.id})`);
 
         const edit = document.createElement("span");
         edit.textContent = "edit_note";
-        edit.classList.add("edit");
-        edit.classList.add("material-symbols-outlined");
-        edit.setAttribute("onclick", `editarTarefa(${tarefas[i].id})`)
+        edit.classList.add("edit", "material-symbols-outlined");
+        edit.setAttribute("onclick", `editarTarefa(${tarefa.id})`);
 
         const deletar = document.createElement("span");
         deletar.textContent = "delete";
-        deletar.classList.add("delete");
-        deletar.classList.add("material-symbols-outlined");
-        deletar.setAttribute("onclick", `deletarTarefa(${tarefas[i].id})`)
+        deletar.classList.add("delete", "material-symbols-outlined");
+        deletar.setAttribute("onclick", `deletarTarefa(${tarefa.id})`);
 
         const div = document.createElement("div");
-        div.style.display = 'flex'
-        div.style.marginTop = '2px'
-        
-        div.appendChild(concluir)
-        div.appendChild(edit)
-        div.appendChild(deletar)
+        div.style.display = 'flex';
+        div.style.marginTop = '2px';
+
+        div.appendChild(concluir);
+        div.appendChild(edit);
+        div.appendChild(deletar);
 
         li.appendChild(span);
         li.appendChild(div);
 
         listaTarefas.appendChild(li);
-    }
+    });
 }
 
 function trocaConcluir(id) {
     const index = tarefas.findIndex(tarefa => tarefa.id === id);
-    const valorAtual = tarefas[index].completed;
-    tarefas[index].completed = !valorAtual;
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    tarefas[index].completed = !tarefas[index].completed;
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
     render();
 }
 
@@ -82,26 +78,26 @@ function editarTarefa(id) {
 
     if (novoTextoTarefa !== null && novoTextoTarefa.trim() !== '') {
         tarefas[index].text = novoTextoTarefa;
-        localStorage.setItem("tarefas", JSON.stringify(tarefas))
+        localStorage.setItem("tarefas", JSON.stringify(tarefas));
         render();
     }
 }
 
-function deletarTarefa(id){
+function deletarTarefa(id) {
     tarefas = tarefas.filter(tarefa => tarefa.id !== id);
-    localStorage.setItem("tarefas", JSON.stringify(tarefas))
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
     render();
 }
 
-function addPeloEnter(evento){
-    if(evento.key === 'Enter'){
+function addPeloEnter(event) {
+    if (event.key === 'Enter') {
         adicionarTarefa();
     }
 }
 
 function carregarTarefas() {
     const tarefasLocalStore = localStorage.getItem("tarefas");
-    if(tarefasLocalStore){
+    if (tarefasLocalStore) {
         tarefas = JSON.parse(tarefasLocalStore);
         render();
     }
